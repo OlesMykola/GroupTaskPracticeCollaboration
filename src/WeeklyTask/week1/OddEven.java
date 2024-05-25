5cd901d257c928fcb51c862e37c8d15121fa9ec9
olesm <olesmykolamud@gmail.com>
2024-05-25 14:50:07 -0500

    week1 task solutions

---
+++ src/WeeklyTask/week1/OddEven.java
@@ -0,0 +1,28 @@
+package WeeklyTask.week1;
+
+public class OddEven {
+
+        public static void main(String[] args) {
+            System.out.println(identify(5));
+            System.out.println(identify(6));
+        }
+
+        public static String identify(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
+    }
+/*
+Task 1
+Numbers -- odd&even
+
+Write a method which can identifies given number is even or odd
+
+EX:
+	identify(5) -> "Odd"
+	identify(6) -> "Even"
+ */
+
