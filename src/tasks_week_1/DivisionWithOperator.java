c4f371a0d18bea88e9dd24452a35566a934e9905
DariaSavchenko12 <pogorelskykhdaria@gmail.com>
2024-05-30 11:26:50 -0400

    create packaged named task_week_1

---
+++ src/tasks_week_1/DivisionWithOperator.java
@@ -0,0 +1,11 @@
+package tasks_week_1;
+
+public class DivisionWithOperator {
+    public static void main(String[] args) {
+
+    }
+}
+
+//Numbers -- Divide without/operator
+//Write a method than can divide two numbers without using division operator
+//
---
+++ src/tasks_week_1/OddEven.java
@@ -0,0 +1,17 @@
+package tasks_week_1;
+
+public class OddEven {
+    public static void main(String[] args) {
+
+    }
+    /*
+Task 1
+Numbers -- odd&even
+
+Write a method which can identifies given number is even or odd
+
+EX:
+	identify(5) -> "Odd"
+	identify(6) -> "Even"
+ */
+}
