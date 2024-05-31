8f491ce7de33956ba3fdd7fc27acfbc2040711ec
stars2026 <rad_etebet@hotmail.com>
2024-05-31 07:25:44 -0400

    Changes made to Rad Week1 completed task

--- src/Rad/Week1/DivisionNoOperator.java
+++ src/Rad/Week1/DivisionNoOperator.java
@@ -4,11 +4,16 @@ public class DivisionNoOperator {
     public static void main(String[] args) {
 
      int dividend = 10;
+
      int divisor = 2;
+
      int quotient = 0;
 
      while (dividend >= divisor){
+
          dividend -= divisor;
+         //
+
          quotient++;
      }
 
--- src/Rad/Week1/OddOrEven.java
+++ src/Rad/Week1/OddOrEven.java
@@ -10,8 +10,11 @@ public class OddOrEven {
     }
 
     public static void OddOrEven(int number){
+
         if (number % 2 == 0){
+
             System.out.println("Even");
+
         }else{
             System.out.println("Odd");
         }
