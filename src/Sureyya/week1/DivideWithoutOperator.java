f82c34e312aa9c9c61544df6c8f87f9c717ae9cc
S-trkmn <sureyyaturkmen4@gmail.com>
2024-06-01 12:36:44 -0700

    completed week1 tasks

---
+++ src/Sureyya/week1/DivideWithoutOperator.java
@@ -0,0 +1,18 @@
+package Sureyya.week1;
+
+public class DivideWithoutOperator {
+    public static void main(String[] args) {
+        int dividend = 1000, divisor = 10 ,HowManyTimes = 0;
+
+        if (divisor == 0){
+            System.out.println("Cannot division by zero");
+            return;
+        }
+
+        while (dividend >= divisor) {
+            HowManyTimes++;
+            dividend -= divisor;
+        }
+        System.out.println( HowManyTimes);
+    }
+}
