e336869dded3c54a8d61356f7e40ad5a0e813eb4
feyzanurdogan <feyza.nur.dogan@hotmail.com>
2024-06-01 20:37:06 -0500

    added lines

--- src/Feyza/week1/divideWithoutOperator.java
+++ src/Feyza/week1/divideWithoutOperator.java
@@ -4,5 +4,7 @@ public class divideWithoutOperator {
     public static void main(String[] args) {
         //i don't know how to solve it :D
         //more things
+        //I want to get a job
+        //I hope we all find a good one. like 120k $
     }
 }
