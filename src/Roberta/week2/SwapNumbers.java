6b61539b5adae3235de95b606909a7f7babd5b02
robertajovaisa <roberta.jovaisa@gmail.com>
2024-06-07 21:28:21 -0500

    completed third task

--- src/Roberta/week2/SwapNumbers.java
+++ src/Roberta/week2/SwapNumbers.java
@@ -8,7 +8,15 @@ public class SwapNumbers {
         Swap two variable values without using a third variable
          */
 
+        int num1 = 2;
+        int num2 = 10;
 
+        num2 = num2-num1;   //8
+        num1 = num1+num2;   //10
+        num2 = num1-num2;   //2
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
 
 
     }
