da7741e0ea43bcdabc1480ea6e6eeef0bb3b91e5
Ihor0777 <ihor.bozhemskyy@gmail.com>
2024-06-14 18:58:00 -0500

    added an if statement

--- src/ihor/week3/ReverseNegativeNumber.java
+++ src/ihor/week3/ReverseNegativeNumber.java
@@ -3,12 +3,15 @@ package ihor.week3;
 public class ReverseNegativeNumber {
     public static void main(String[] args) {
 
-        System.out.println("reverseNegative(-4352) = " + reverseNegative(-4352));
+        System.out.println("reverseNegative(25) = " + reverseNegative(-2597));
 
     }
 
         public static String reverseNegative(int num) {
             String numAsString = String.valueOf(num);
+            if (!numAsString.contains("-")){
+                return "The number must be negative";
+            }
             String reversed = "";
             for (int i = numAsString.length()-1; i > 0 ; i--) {
                 reversed += numAsString.charAt(i);
